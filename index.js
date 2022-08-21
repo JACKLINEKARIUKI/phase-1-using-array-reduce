53933ecafd51095f439048035bb7eca2f601884e
JACKLINEKARIUKI <jacklinewk71@gmail.com>
2022-08-21 15:35:50 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
-
+var totalBatteries = 0;
 // Code your solution here
+
+function reducer (array) {
+  totalBatteries = array.reduce((sum, element) => sum += element, totalBatteries)
+}
+
+reducer(batteryBatches)
